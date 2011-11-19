07edc8fc5d04b4f8f80b06011026a022bb254768
soul4soul <soul4soul@localhost>
2011-11-19 12:18:36 -0500

    added a comment to fire_bug.py

--- data/scripts/actions/fire_bug.py
+++ data/scripts/actions/fire_bug.py
@@ -1,6 +1,6 @@
 thingId = 5466
 fireBugs = 5468
-
+#need to be able to burn spider webs and light empty coal basins
 def onUse(creature, position, stackpos, onId, onThing, onPosition, onStackpos, **k):
     if onId == 5466: # Sugar cane
         if(random.randint(0,20) == 0):
