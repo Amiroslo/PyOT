1728fb4f85aa87d728709abb92b121a0a976f47d
stian <stian@localhost>
2014-10-20 18:50:33 +0200

    Fix reenable cffi for cryptography

--- game/otcrypto.py
+++ game/otcrypto.py
@@ -48,7 +48,7 @@ def bytes( long_int ):
 def decryptRSA(stream):
     return bytes(pow(int.from_bytes(stream, 'big'), D, N))
 
-if ffi and False:
+if ffi:
     def decryptXTEA(stream, k):
         length = len(stream) >> 2
         bstr = "<%dL" % length
